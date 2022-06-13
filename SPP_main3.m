1abe48d4ff5927fb08687bef6238ecd629c92606
cesarcastano2 <castanocesar@knights.ucf.edu>
2022-06-13 13:33:27 -0400

    added path to NAS

--- SPP_main3.m
+++ SPP_main3.m
@@ -17,7 +17,7 @@ conds = {'no_pert' 'same_mf' 'diff_f' 'diff_m' 'diff_fm'};
 
 fs = 240; %hz, resamp tm to df rate
 % proj = 'E:\SPP\Subjects\';
-proj = 'D:\cesar\SPP\Subjects\'; %lab computer 
+proj = 'X:\SPP\subjects'; %NAS computer 
 
 
 for s = 1:length(subjs)
