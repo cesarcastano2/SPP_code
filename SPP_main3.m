cf7c9f5e74b298d230d035106c1d22fee9ca2503
cesarcastano2 <castanocesar@knights.ucf.edu>
2022-06-13 13:40:29 -0400

    troubleshoot

--- SPP_main3.m
+++ SPP_main3.m
@@ -16,8 +16,8 @@ conds = {'no_pert' 'same_mf' 'diff_f' 'diff_m' 'diff_fm'};
 
 
 fs = 240; %hz, resamp tm to df rate
-% proj = 'E:\SPP\Subjects\';
-proj = 'X:\SPP\subjects'; %NAS computer 
+proj = 'E:\SPP\Subjects\';
+% proj = 'X:\SPP\subjects'; %NAS computer
 
 
 for s = 1:length(subjs)
